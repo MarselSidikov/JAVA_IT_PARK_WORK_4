1e859bc45f7b140027549d19a96ac484a808740c
student21 <student21@itpark.ru>
2017-10-12 20:57:27 +0300

    characters

---
+++ Themes/Characters/src/ru/itpark/Main.java
@@ -0,0 +1,14 @@
+package ru.itpark;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    char a = 'A';
+	    int aCode = a;
+        System.out.println(aCode);
+        char b = (char)(aCode + 1);
+        System.out.println(b);
+        char ы = 'Ы';
+        System.out.println((int)ы);
+    }
+}
