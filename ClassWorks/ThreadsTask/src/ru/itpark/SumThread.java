c34764b851bc317374804abc70296bb3b4af70e9
student21 <student21@itpark.ru>
2017-11-23 20:57:20 +0300

    add thread

--- ClassWorks/ThreadsTask/src/ru/itpark/SumThread.java
+++ ClassWorks/ThreadsTask/src/ru/itpark/SumThread.java
@@ -1,8 +1,13 @@
 package ru.itpark;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class SumThread extends Thread {
   private int start;
   private int end;
+
+  private final static Lock lock = new ReentrantLock();
 
   public SumThread(int start, int end) {
     this.start = start;
@@ -11,7 +16,13 @@ public class SumThread extends Thread {
 
   public void run() {
     for (int i = start; i < end; i++) {
+//      synchronized (Main.array) {
+//        Main.result = Main.result + Main.array[i];
+//      }
+      lock.lock();
       Main.result = Main.result + Main.array[i];
+      lock.unlock();
+
     }
   }
 }
